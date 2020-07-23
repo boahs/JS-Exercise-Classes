3ab4d233f636f5ad99f73bd8776597df38e7e7d3
boahs <39522778+boahs@users.noreply.github.com>
2020-07-23 16:13:04 -0400

    task 6

--- index.js
+++ index.js
@@ -200,10 +200,23 @@ class Student extends Lambdasian {
     - Its constructor calls the parent constructor passing to it what it needs.
     - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
     - ProjectManager instances have the following methods:
-        + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
-        + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
+        + `standUp` a method that takes in a slack channel and 
+        returns `{name} announces to {channel}, @channel standy times!`
+        + `debugsCode` a method that takes in a student object and
+         a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor(arg){
+    super(arg)
+    this.gradClassName = arg.gradClassName;
+    this.favInstructor = arg.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 
 }
 
